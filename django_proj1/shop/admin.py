86332b4225f7bb27edf88c4bc669dd928fa3790a
gmrdns03 <dnsdk03@gmail.com>
2021-08-05 14:35:55 +0900

    item에 리뷰 항목 만들기

--- django_proj1/shop/admin.py
+++ django_proj1/shop/admin.py
@@ -1,3 +1,4 @@
+from shop.models import Review
 from django.contrib import admin
 from shop.models import Item
 
@@ -9,4 +10,10 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ["name"]
     list_display = ["name", "desc", "price", "is_public", "created_at", "updated_at"]
     list_filter = ["is_public", "created_at"]
+
+
+@admin.register(Review)
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ["item"]
+    list_display = ["item", "message", "created_at", "updated_at"]
 
--- django_proj1/shop/models.py
+++ django_proj1/shop/models.py
@@ -1,11 +1,24 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(
+        max_length=100, db_index=True, validators=[MinLengthValidator(3)]
+    )
     desc = models.TextField()
     photo = models.ImageField(blank=True)
     price = models.PositiveIntegerField()
     is_public = models.BooleanField(default=False, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    message = models.TextField()
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
