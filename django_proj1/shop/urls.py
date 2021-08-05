5de0e93946f783b44a6ccc12cc9a1b52b38d9b0b
gmrdns03 <dnsdk03@gmail.com>
2021-08-05 16:24:17 +0900

    item리뷰와 이미지 html에 목록 구축하기

--- django_proj1/shop/urls.py
+++ django_proj1/shop/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from shop import views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("<int:pk>/", views.index, name="index"),
+]
 
--- django_proj1/shop/views.py
+++ django_proj1/shop/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from shop.models import Item
+from django.http import HttpRequest
 
 # Create your views here.
+def index(request: HttpRequest):  # pk는 프라이머리키의 약자
+    qs = Item.objects.all()
+    return render(request, "shop/item_list.html", {"item_list": qs,},)
+
